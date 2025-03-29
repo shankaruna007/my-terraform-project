8bad58b6ed7f099330e9cfc78d98c7c291aaef92
shankaruna007 <shankarunakara3@gmail.com>
2025-03-29 22:32:44 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Key name for EC2 instance SSH"
+  type        = string
+}
