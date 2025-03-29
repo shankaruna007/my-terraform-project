db77a24679341a21dda60d89a528e543deef910a
shankaruna007 <shankarunakara3@gmail.com>
2025-03-29 22:34:03 +0530

    Create ec2_instance.tf

---
+++ ec2_instance.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "test_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  tags = {
+    Name = "TestEC2Instance"
+  }
+}
+
+resource "aws_security_group" "sg" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.test_instance.id
+}
+
+output "public_ip" {
+  value = aws_instance.test_instance.public_ip
+}
