e09c5e79724e16fc9eae8e16cef7b1f084a3b365
shankaruna007 <shankarunakara3@gmail.com>
2025-03-30 17:14:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,28 +1,11 @@
 variable "aws_region" {
-  description = "The AWS region to deploy resources"
+  description = "AWS region"
   type        = string
   default     = "us-east-1"
 }
 
-variable "vpc_cidr_block" {
-  description = "CIDR block for the VPC"
+variable "vpc_cidr" {
+  description = "VPC CIDR Block"
   type        = string
   default     = "10.0.0.0/16"
-}
-
-variable "ami_id" {
-  description = "AMI ID for EC2 instance"
-  type        = string
-  default     = "ami-071226ecf16aa7d96"
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t2.micro"
-}
-
-variable "key_name" {
-  description = "Key name for EC2 instance SSH"
-  type        = string
 }
