786dda16daf9b48736f3d6b1a0dad6c1bd2e5499
shankaruna007 <shankarunakara3@gmail.com>
2025-03-30 17:14:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,22 @@
-provider "aws" {
-  region = var.aws_region
-}
-
 module "vpc" {
-  source     = "./modules/vpc"
-  vpc_cidr   = var.vpc_cidr
+  source   = "./modules/vpc"
+  vpc_cidr = var.vpc_cidr
 }
 
 module "subnet" {
-  source      = "./modules/subnet"
-  vpc_id      = module.vpc.vpc_id
-  subnet_cidr = var.subnet_cidr
+  source   = "./modules/subnet"
+  vpc_id   = module.vpc.vpc_id
+  cidr     = "10.0.1.0/24"
 }
 
 module "security_group" {
-  source       = "./modules/security-group"
-  vpc_id       = module.vpc.vpc_id
+  source  = "./modules/security-group"
+  vpc_id  = module.vpc.vpc_id
 }
 
 module "ec2" {
-  source           = "./modules/ec2"
-  ami_id           = var.ami_id
-  instance_type    = var.instance_type
-  subnet_id        = module.subnet.subnet_id
-  security_group_id = module.security_group.security_group_id
+  source            = "./modules/ec2"
+  instance_type     = "t3.micro"
+  subnet_id         = module.subnet.subnet_id
+  security_group_id = module.security_group.sg_id
 }
