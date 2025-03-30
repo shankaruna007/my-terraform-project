a24bdbdaa5102d320f7213fea49170e2a059f445
shankaruna007 <shankarunakara3@gmail.com>
2025-03-30 17:30:51 +0530

    Create main.tf

---
+++ modules/security-group/main.tf
@@ -0,0 +1,13 @@
+resource "aws_security_group" "this" {
+  vpc_id = var.vpc_id
+
+  dynamic "ingress" {
+    for_each = var.allowed_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = var.allowed_cidrs
+    }
+  }
+}
