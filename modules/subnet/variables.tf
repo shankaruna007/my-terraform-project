f0835503c4639cc3d8a0fdc41b70535cebfafd41
shankaruna007 <shankarunakara3@gmail.com>
2025-03-30 13:40:09 +0000

    jj

--- modules/subnet/variables.tf
+++ modules/subnet/variables.tf
@@ -1,3 +1,9 @@
-variable "vpc_id" {}
-variable "subnet_cidr_block" {}
-variable "availability_zone" {}
+variable "allowed_ports" {
+  description = "List of allowed ports in the security group"
+  type        = list(number)
+}
+
+variable "allowed_cidrs" {
+  description = "List of allowed CIDR blocks"
+  type        = list(string)
+}
