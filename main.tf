d8486760ef53e205d898be46b4151c3994dec09c
shankaruna007 <shankarunakara3@gmail.com>
2025-03-30 17:22:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,27 @@
 module "vpc" {
-  source   = "./modules/vpc"
-  vpc_cidr = var.vpc_cidr
+  source        = "./modules/vpc"
+  vpc_cidr_block = var.vpc_cidr_block
+  vpc_name      = var.vpc_name
 }
 
 module "subnet" {
-  source   = "./modules/subnet"
-  vpc_id   = module.vpc.vpc_id
-  cidr     = "10.0.1.0/24"
+  source           = "./modules/subnet"
+  vpc_id           = module.vpc.vpc_id
+  subnet_cidr_block = var.subnet_cidr_block
+  availability_zone = var.availability_zone
 }
 
 module "security_group" {
-  source  = "./modules/security-group"
-  vpc_id  = module.vpc.vpc_id
+  source        = "./modules/security-group"
+  vpc_id        = module.vpc.vpc_id
+  allowed_ports = var.allowed_ports
+  allowed_cidrs = var.allowed_cidrs
 }
 
 module "ec2" {
   source            = "./modules/ec2"
-  instance_type     = "t2.micro"
+  ami_id            = var.ami_id
+  instance_type     = var.instance_type
   subnet_id         = module.subnet.subnet_id
-  security_group_id = module.security_group.sg_id
+  security_group_id = module.security_group.security_group_id
 }
