0d85948f629a8f2e889b9e11528c4e8165a46227
shankaruna007 <shankarunakara3@gmail.com>
2025-03-30 17:32:09 +0530

    Create variables.tf

---
+++ modules/variables.tf
@@ -0,0 +1,19 @@
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "Security Group ID"
+  type        = string
+}
