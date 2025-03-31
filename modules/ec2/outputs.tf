130c51ef3016420199c0adeb6514b49d19ff8ca8
shankaruna007 <shankarunakara3@gmail.com>
2025-03-31 16:16:44 +0000

    cmt

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,3 +1,8 @@
 output "instance_id" {
   value = aws_instance.this.id
 }
+
+output "public_ip" {
+  description = "The public IP address of the instance"
+  value       = aws_instance.this.public_ip
+}
