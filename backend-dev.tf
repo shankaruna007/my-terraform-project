544b23c0cf24ca6310059a128a43c88841026747
shankaruna007 <shankarunakara3@gmail.com>
2025-04-07 23:12:33 +0530

    Update and rename backend.tf to backend-dev.tf

--- backend-dev.tf
+++ backend-dev.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-kk-tf-state"
-    key            = "terraform/terraform.tfstate"
+    key            = "terraform/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "my-tf-state-lock-table"
     encrypt        = true
