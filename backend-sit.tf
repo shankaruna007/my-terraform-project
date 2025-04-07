db074ddcdf36066b106dc18151dc1117378303d3
shankaruna007 <shankarunakara3@gmail.com>
2025-04-07 23:13:15 +0530

    Create backend-sit.tf

---
+++ backend-sit.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-kk-tf-state"
+    key            = "terraform/sit/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "my-tf-state-lock-table"
+    encrypt        = true
+  }
+}
